513cea7960d598ab76a81fc362727fed736a4e94
Kateryna Degtyariova <katya.d@gmail.com>
2021-03-05 11:36:15 +1100

    Added check if users table exists and is compatible

--- user_upload.php
+++ user_upload.php
@@ -7,6 +7,14 @@
  * March 2021
  */
 
+/*
+ * TODO:
+ *    Incomplete CSV with the number of columns less than 3 or empty values
+ *    Checking if 'users' table already exists and has columns
+ *    Data warning when more than 3 columns in CSV
+ */
+
+
 $term_colors = array(
   'RED'  => "\x1B[31m",
   'GRN'  => "\x1B[32m",
@@ -65,18 +73,20 @@ function statmsg($msg, $stdout = FALSE)
   if ($show_status == FALSE)
     return;
   
-  $out = "";
+  $out = $term_colors['YEL']."STATUS: ".$term_colors['RESET'];
   
   if (is_array($msg))
   {
+    $out = $term_colors['CYN']."RESULT: ".$term_colors['RESET'];
     foreach($msg as $k => $v)
       $out .= $term_colors['CYN']."[ $k : ".$term_colors['RESET'].$v.
               $term_colors['CYN']." ] ".$term_colors['RESET'];
   }
   else
-    $out = $msg;
+    $out .= $msg;
   
-  $out = $term_colors['YEL']."STATUS: ".$term_colors['RESET'].$out."\n";
+  $out .= "\n";
+  
   if ($stdout)
     fwrite(STDOUT, $out);
   else
@@ -291,11 +301,13 @@ class CsvUpload
     
     echo " --file <csv file name> – the name of the CSV to be parsed\n".
          " --create_table - this will cause the PostgreSQL users table to be \n".
-         "                  built (and no further action will be taken)\n".
+         "                  built (and no further action will be taken).\n".
+         "                  If users table already exists no action will be\n".
+         "                  taken\n".
          " --dry_run      - this will be used with the --file directive in case\n".
          "                  we want to run the script but not insert into\n".
          "                  the DB. All other functions will be executed,\n".
-         "                  but the database won\'t be altered. The status\n".
+         "                  but the database won't be altered. The status\n".
          "                  and validated data will be printed.\n".
          " -u <username>  - user name to connect to PostgreSQL\n".
          " -p <password>  - password to connect to PostgreSQL\n".
@@ -368,9 +380,73 @@ class CsvUpload
     return TRUE;
   }
   
+
+  /*
+   * Checks if table 'users' exists before trying to insert new rows.
+   * Checks if table 'users' is compatible: it should contain the columns:
+   *  - name
+   *  - surname
+   *  - email
+   * 
+   * Columns maximum character length must be larger than 127 characters.
+   * 
+   * NOTE: it does not check for the uniqueness of email column. It should be
+   *       enough that the column exists and has the sufficient size
+   *       to store values.
+   * 
+   * Returns:
+   *   TRUE - table exists
+   *   FALSE - table does not exist or is incompatible or error happened
+   *
+   */
+  function check_users_table()
+  {
+    try
+    {
+      $res = $this->conn->query("SELECT t1.column_name AS col_name, 
+        t2.column_name AS col_surname, t3.column_name as col_email 
+      FROM information_schema.columns t1 
+      LEFT JOIN
+        information_schema.columns t2 ON t1.table_name = t2.table_name
+      LEFT JOIN
+        information_schema.columns t3 ON t1.table_name = t3.table_name
+      WHERE t1.table_name='users' AND t1.table_catalog=current_catalog 
+        AND t1.table_schema=current_schema
+        AND t1.column_name='name'
+        AND t2.column_name='surname'
+        AND t3.column_name='email'
+        AND t1.character_maximum_length > 127
+        AND t2.character_maximum_length > 127
+        AND t3.character_maximum_length > 127");
+      
+      $row = $res->fetch();
+      
+      // Check if there is anything wrong with the row we have obtained
+      if (!$row || !isset($row['col_name']) ||
+          !isset($row['col_surname']) ||
+          !isset($row['col_email']) ||
+          strcmp($row['col_name'], 'name') != 0 ||
+          strcmp($row['col_surname'], 'surname') != 0 ||
+          strcmp($row['col_email'], 'email') != 0)
+        return FALSE;
+      
+      // Unlikely there are any other rows, but finish them anyway
+      while($res->fetch()) {}
+      
+    }
+    catch (PDOException $ex)
+    {
+      errmsg($ex->getMessage());
+      return FALSE;
+    }
+    
+    return TRUE;
+  }
+
   
   /* 
-   * Creates a new users table into the database
+   * Creates a new 'users' table into the database.
+   * It will not do anything if table with that name already exists.
    * 
    * Returns:
    *  0 - Success
@@ -402,7 +478,8 @@ class CsvUpload
   
   
   /*
-   * Inserting one row of data into 'users' table
+   * Inserting one row of data into 'users' table.
+   * Uses prepared statement with parameters to protect against SQL injection.
    * 
    * Parameters:
    *   data - array, data to insert
@@ -463,6 +540,13 @@ class CsvUpload
     if (!$this->connect())
       return 1;
     
+    if ($this->check_users_table() == FALSE)
+    {
+      errmsg("The table 'users' does not exist or is incompatible with ".
+             "the required format. Use --create_table directive to create ".
+             "the 'users' table");
+    }
+    
     try
     {
       $this->csv_file = new CsvFile($fname);
@@ -498,6 +582,8 @@ class CsvUpload
         $processed_count++; // For dry_run just increment
     }
     infomsg("Processed $processed_count out of ".$this->csv_file->line_num." lines");
+    if ($dry_run)
+      infomsg("No changes are made in the database");
     return 0;
   }
   
